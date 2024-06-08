7730291e06f0de96a2e151fcf1696f1cd1785d62
Reach2Mustafa <mohammedb4u2@gmail.com>
2024-06-08 18:53:06 +0530

    done

--- public/icons/arrowleft.js
+++ public/icons/arrowleft.js
@@ -1,12 +1,12 @@
 import React from 'react'
 
-const Arrowleft = () => {
+const Arrowleft = ({ color }) => {
   return (
     <div>
-      <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-<path d="M9.57 5.92993L3.5 11.9999L9.57 18.0699" stroke="#292D32" stroke-width="1.5" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"/>
-<path d="M20.5019 12H3.67188" stroke="#292D32" stroke-width="1.5" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"/>
-</svg>
+      <svg width="20" height="20" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M9.57 5.92993L3.5 11.9999L9.57 18.0699" stroke={color || "#292D32"} stroke-width="1.5" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round" />
+        <path d="M20.5019 12H3.67188" stroke={color || "#292D32"} stroke-width="1.5" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round" />
+      </svg>
 
     </div>
   )
--- sections/home/people.js
+++ sections/home/people.js
@@ -1,11 +1,29 @@
 import Padding from '@/components/padding'
+import Arrowleft from '@/public/icons/arrowleft'
+import Image from 'next/image'
 import React from 'react'
-
+import People1 from "../../public/images/people1.jpg"
 const People = () => {
     return (
-        <div className=' w-full h-[100vh]'>
-            <Padding>
-
+        <div className=' w-full  py-[5rem]'>
+            <Padding className={" w-full flex h-full"}>
+                <div>
+                    <div className=' uppercase font-thunder tracking-wide text-[5rem] w-[400px] leading-none font-[700]'>
+                        For people
+                        DOCTORS AND INSTITUTIONS
+                    </div>
+                    <div className=' flex gap-[1rem] py-[1rem]'>
+                        <div className=' bg-[#F7F8FA] border-[1px] border-[#E8E8EA] w-max p-3 px-4 rounded-full'>
+                            <Arrowleft color={"#8A8F95"} />
+                        </div>
+                        <div className=' bg-[#9FE870] rotate-180 border-[1px] border-[#9FE870] w-max p-3 px-4 rounded-full'>
+                            <Arrowleft />
+                        </div>
+                    </div>
+                </div>
+                <div className=' h-[80%] w-full'>
+                    <Image className=' h-[100%] object-cover w-[300px]' src={People1} />
+                </div>
             </Padding>
         </div>
     )
