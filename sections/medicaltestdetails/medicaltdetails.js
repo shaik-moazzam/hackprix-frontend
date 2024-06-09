bd2109ae74e472d5d5f18ffc90038c4a5d475f42
shaik-moazzam <shaikmoazzam1@gmail.com>
2024-06-09 06:19:37 +0530

    done

---
+++ sections/medicaltestdetails/medicaltdetails.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Medicaltdetails = () => {
+  return (
+    <div className=" p-[3rem] bg-[#F7F7F7] ">
+      <div>Laboratory Results:</div>
+      <div className=" bg-[#FFFFFF] ">
+        <div>Fasting Blood Glucose:</div>
+        <div>Fasting Blood Glucose:</div>
+      </div>
+    </div>
+  );
+};
+
+export default Medicaltdetails;
